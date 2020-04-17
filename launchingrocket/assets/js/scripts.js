d3adeea1eea94c973cc24698871debdba57c345c
Camo Aegis <kmoart@gmail.com>
2020-04-16 22:39:26 -0500

    Adición de web Rocket

---
+++ launchingrocket/assets/js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+      $(".button-collapse").sideNav();
+});
+
+$(document).ready(function(){
+      $('.parallax').parallax();
+});
+
+$(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+});
+
+$('.fixed-action-btn').openFAB();
+  $('.fixed-action-btn').closeFAB();
+  $('.fixed-action-btn.toolbar').openToolbar();
+  $('.fixed-action-btn.toolbar').closeToolbar();
+
+
